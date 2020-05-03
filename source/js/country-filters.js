da8b4520c7779c737bc534313f17b0692ab45f62
Anastasia Titova <Kutsanastasia@yandex.ru>
2020-05-04 03:48:25 +0700

    правки страница catalog

--- source/js/country-filters.js
+++ source/js/country-filters.js
@@ -1,13 +1,13 @@
 var filters = document.querySelector(".country-filters__show");
 var world = document.querySelector(".country-filters__part-world")
-var toggler = document.querySelector(".country-filters__toggler");
+var togglers = document.querySelector(".country-filters__toggler");
 var open = document.querySelector(".toggler__text--open");
 var close = document.querySelector(".toggler__text--close");
 
-toggler.addEventListener("click", function (evt) {
+togglers.addEventListener("click", function (evt) {
   event.preventDefault();
-  toggler.classList.toggle("toggler--open");
-  toggler.classList.toggle("toggler--close");
+  togglers.classList.toggle("toggler--open");
+  togglers.classList.toggle("toggler--close");
   filters.classList.toggle("country-filters__show--close");
   filters.classList.toggle("country-filters__show--open");
   world.classList.toggle("country-filters__part-world--close");
