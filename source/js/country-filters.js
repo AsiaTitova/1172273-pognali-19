1f8909b72406decac67114af0181406e15502741
Anastasia Titova <Kutsanastasia@yandex.ru>
2020-05-18 08:48:52 +0700

    правки по критериям к проекту

--- source/js/country-filters.js
+++ source/js/country-filters.js
@@ -1,16 +1,14 @@
-var filters = document.querySelector(".country-filters__show");
-var world = document.querySelector(".country-filters__part-world")
+var filters = document.querySelector(".country-filters__container");
+
+if (filters) {
+  filters.classList.add("country-filters__container--close");
+}
+
 var togglers = document.querySelector(".country-filters__toggler");
-var open = document.querySelector(".toggler__text--open");
-var close = document.querySelector(".toggler__text--close");
 
 togglers.addEventListener("click", function (evt) {
   event.preventDefault();
   togglers.classList.toggle("toggler--open");
   togglers.classList.toggle("toggler--close");
-  filters.classList.toggle("country-filters__show--close");
-  filters.classList.toggle("country-filters__show--open");
-  world.classList.toggle("country-filters__part-world--close");
-  open.classList.toggle("toggler__text--show");
-  close.classList.toggle("toggler__text--show");
+  filters.classList.toggle("country-filters__container--close");
 });
--- source/js/menu.js
+++ source/js/menu.js
@@ -1,8 +1,22 @@
 var menu = document.querySelector(".main-navigation");
 
+var x = function (menu) {
+  if (menu) {
+  menu.classList.remove("main-navigation--open");
+  menu.classList.remove("main-navigation--cross");
+  }
+}
+
 var toggle = document.querySelector(".main-navigation__toggler");
 
 toggle.addEventListener("click", function (evt) {
   event.preventDefault();
   menu.classList.toggle("main-navigation--open");
+  menu.classList.toggle("main-navigation--absolute");
 });
+
+document.onscroll = function(){
+  var fix = document.querySelector("#scroll").getBoundingClientRect().top;
+  menu.classList.add("main-navigation--fixed", fix <= window.innerHeight
+   );
+}
